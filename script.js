b43dfe58addc6e15d4310e254e703f21faccc901
alebaviera <baviera.alessandra@gmailcom>
2019-08-31 15:40:27 +0200

    function addSale() (non esegue il parseInt!)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function init() {
 
     getDataServer();
+    $('#addSale').click(addSale);
+    // postDataServer();
 }
 
 function getLabelMonth(){
@@ -21,7 +23,7 @@ function getDataSales(data){
 
   for (var i = 0; i < data.length; i++) {
     var d = data[i];
-    var amount = d.amount;
+    var amount = Number(d.amount);
     var monthSales = d.date;
     var month = moment(monthSales, 'DD/MM/YYYY').month();
     console.log(month);
@@ -42,7 +44,7 @@ function getDataSeller(data){
   for (var i = 0; i < data.length; i++) {
     var d = data[i];
     var seller = d.salesman;
-    var amount = d.amount;
+    var amount = Number(d.amount);
 
 
 
@@ -63,6 +65,113 @@ function getDataSeller(data){
 
 }
 
+function selSeller(data){
+  var sellers = Object.keys(getDataSeller(data));
+  console.log(sellers);
+  for (var i = 0; i < sellers.length; i++) {
+      $("#selSeller").append($('<option>', {
+    value: sellers[i],
+    text: sellers[i]
+      }));
+  }
+}
+
+function selMonth(data){
+  var months = getLabelMonth();
+  console.log(months);
+  for (var i = 0; i < months.length; i++) {
+      $("#selMonth").append($('<option>', {
+    value: months[i],
+    text: months[i]
+      }));
+  }
+}
+
+function printLineChart(data){
+  var monthSales = getDataSales(data);
+  var months = getLabelMonth();
+  console.log(monthSales, months);
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: months,
+          datasets: [{
+              label: '# Sales',
+              data: monthSales,
+
+
+          }]
+      },
+
+  });
+}
+
+function printPieChart(data){
+  var sellers = getDataSeller(data);
+  var nameSalesman = Object.keys(sellers);
+  var amountSeller = Object.values(sellers);
+
+  //calcolo i valori in %
+
+  var totAmount = amountSeller.reduce(add);
+  console.log(totAmount);
+  for (var i = 0; i < amountSeller.length; i++) {
+    amountSeller[i] = Math.floor((amountSeller[i]/totAmount)*100);
+  }
+  console.log(amountSeller);
+  var ctxDoughnut = document.getElementById('myChartDoughnut').getContext('2d');
+  var myChartDoughnut = new Chart(ctxDoughnut, {
+      type: 'doughnut',
+      data: {
+          labels: nameSalesman,
+          datasets: [{
+              label: '# Sales',
+              data: amountSeller,
+
+
+          }]
+      },
+
+  });
+}
+
+function addSale(){
+
+  var selectedSeller = $('#selSeller').val();
+  console.log(selectedSeller);
+  var selectedMonth = $('#selMonth').val();
+  console.log(selectedMonth);
+  var newAmount = parseInt($('input').val()); // restituisce un numero
+  console.log(newAmount);
+
+
+
+
+  var month = moment().month(selectedMonth).format('MM');
+  console.log(month);
+
+  $.ajax({
+    url:'http://157.230.17.132:4010/sales',
+    method:'POST',
+    data: {
+      amount : parseInt(newAmount), //restituisce una stringa!!
+      salesman : selectedSeller,
+      date : '01/' + month + '/2017'
+    },
+    success: function(data){
+      console.log('postato', data);
+       // getDataServer();
+
+    },
+    error: function(){
+      alert('errore');
+    },
+  });
+  $('input').val('');
+
+}
+
 function getDataServer(){
 
   $.ajax({
@@ -72,50 +181,15 @@ function getDataServer(){
     success: function(data){
       console.log(data);
 
-      var monthSales = getDataSales(data);
-      var months = getLabelMonth();
-      console.log(monthSales, months);
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var myChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-              labels: months,
-              datasets: [{
-                  label: '# Sales',
-                  data: monthSales,
+      printLineChart(data);
+      printPieChart(data);
+      selSeller(data);
+      selMonth(data);
 
 
-              }]
-          },
-
-      });
-
-      var sellers = getDataSeller(data);
-      var nameSalesman = Object.keys(sellers);
-      var amountSeller = Object.values(sellers);
-
-      //calcolo i valori in %
-
-      var totAmount = amountSeller.reduce(add);
-      console.log(totAmount);
-      for (var i = 0; i < amountSeller.length; i++) {
-        amountSeller[i] = Math.floor((amountSeller[i]/totAmount)*100);
-      }
-      console.log(amountSeller);
-      var ctxDoughnut = document.getElementById('myChartDoughnut').getContext('2d');
-      var myChartDoughnut = new Chart(ctxDoughnut, {
-          type: 'doughnut',
-          data: {
-              labels: nameSalesman,
-              datasets: [{
-                  label: '# Sales',
-                  data: amountSeller,
 
 
-              }]
-          },
 
-      });
     },
     error: function(){
       alert('errore');
